6fa6a1146368630f7c1d69063089b84ebbcb6e02
South Africa - Jarrod Benjamin <46967203+JarrodBen@users.noreply.github.com>
2021-06-12 11:13:37 +0200

    added a header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className="header dark-element dark-shadow">
+            <nav>
+                <h2>Where in the World?</h2>
+            </nav>
+        </header>
+    )
+}
